dad64e2d3184223791badfbfbcadac26b5b23608
connorq <connorquitt@gmail.com>
2023-08-27 18:27:00 -0500

    working

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const heart = document.querySelectorAll(".like-glyph")
+const errorMod = document.getElementById('modal')
 
-// Your JavaScript code goes here!
+
+for(const like of heart) {
+like.addEventListener("click", () => {
+  mimicServerCall()
+  .then(() => {
+    if(like.textContent === EMPTY_HEART) {
+      like.textContent = FULL_HEART
+      like.classList.add('activated-heart')
+    }else if(like.textContent === FULL_HEART){
+      like.textContent = EMPTY_HEART
+      like.classList.remove('activated-heart')
+    }
+  })
+  .catch((error) => {
+    errorMod.classList.remove('hidden')
+    errorMod.textContent = error
+    setTimeout(() => errorMod.classList.add("hidden"), 3000)
+    console.log(error)
+  })
+})}
+
 
 
 
